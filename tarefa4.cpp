f746800e8eb99e0359ad69b274ed35509840812e
CruzLeyvaSegundo <informaticatrabajosperu@gmail.com>
2016-04-05 22:55:22 -0300

    Ahora las transformacion solo son para la tortuga y ya esta traducido al portugues

--- tarefa4.cpp
+++ tarefa4.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 
 FILE *fd,*fs;
-int numFil,numCol,grayScale;
+int numFil,numCol,grayScale,mayorX,mayorY,menorX,menorY;
 int matrizImg[900][900],matrizAux[900][900],matrizOriginal[900][900];
 string nomSalida,nomEntrada;
 struct PuntoLimite
@@ -22,11 +22,12 @@ struct PuntoLimite
 	}
    	void rotateP(float t) 
    	{
-   		int cx,cy,coseno,seno,xAux=x;
+   		int cx,cy,xAux=x;
+   		float coseno,seno;
    		coseno=cos(-t*PI/180.0);
    		seno=sin(-t*PI/180.0);
-		cx=numFil/2;
-		cy=numCol/2;
+		cx=(mayorX-menorX)/2 + menorX;
+		cy=(mayorY-menorY)/2  + menorY;
 		x=coseno*(x-cx) - seno*(y-cy) +cx;
 		y=seno*(xAux-cx) + coseno*(y-cy) +cy;
 	} 
@@ -40,9 +41,9 @@ struct PuntoLimite
    	void reflexionP(int op)
 	{ 
 		if(op==1)  // 1 ES HORIZONTAL
-		   	y=numCol-y+1;
+		   	y=2*mayorY-y;
 		else if(op==2) // 2 ES VERTICAL
-		   	x=numFil-x+1;	   			   		
+		   	x=2*mayorX-x;   			   		
 	}
 	bool fueraLimite()
 	{
@@ -51,9 +52,39 @@ struct PuntoLimite
 		else 
 			return false;
 	}
-} lim[4]; 
-
-int lenNumero(int Num) //Calcula el numero de digitos de un numero
+} lim[4]; //Pontos limites para verificar se a tartaruga está totalmente fora do quadro
+void medidas()
+{
+	int m=lim[0].x;
+	for(int i=1;i<4;i++)
+	{
+		if(lim[i].x<m)
+			m=lim[i].x;
+	}
+	menorX=m;
+	m=lim[0].x;
+	for(int i=1;i<4;i++)
+	{
+		if(lim[i].x>m)
+			m=lim[i].x;
+	}
+	mayorX=m;
+	m=lim[0].y;
+	for(int i=1;i<4;i++)
+	{
+		if(lim[i].y>m)
+			m=lim[i].y;
+	}
+	mayorY=m;
+	m=lim[0].y;
+	for(int i=1;i<4;i++)
+	{
+		if(lim[i].y<m)
+			m=lim[i].y;
+	}
+	menorY=m;
+}
+int lenNumero(int Num) //Calcular o número de dígitos de um número
 { 
     int i=0;
     if(Num==0)
@@ -77,34 +108,34 @@ void copiarMatriz(int A[900][900],int B[900][900])
 
 void reflexion(int op)
 {
-	lim[0].reflexionP(op);
-	lim[1].reflexionP(op);
-	lim[2].reflexionP(op);
-	lim[3].reflexionP(op);
+	medidas();
 	int x,y,x2,y2;
-	for(x=1;x<=numFil;x++)
-		for(y=1;y<=numCol;y++)
+	for(x2=1;x2<=numFil;x2++)
+		for(y2=1;y2<=numCol;y2++)
 		{
 		   	if(op==1)  // 1 ES HORIZONTAL
 		   	{
-		   		x2=x;
-		   		y2=numCol-y+1;
+		   		x=x2;
+		   		y=2*mayorY-y2;
 			}
 		   	else if(op==2) // 2 ES VERTICAL
 		   	{
-		   		x2=numFil-x+1;
-		   		y2=y;	   			   		
+		   		x=2*mayorX-x2;
+		   		y=y2;	   			   		
 			}
-		    matrizAux[x2][y2]=matrizImg[x][y];
+		   	if((x>=1) && (y>=1) &&(x<=numFil)&&(y<=numCol))
+		      	matrizAux[x2][y2]=matrizImg[x][y];
+		   	else
+		      	matrizAux[x2][y2]=255;//aqui le pones un valor que quieras
 		}
+	lim[0].reflexionP(op);
+	lim[1].reflexionP(op);
+	lim[2].reflexionP(op);
+	lim[3].reflexionP(op);		
 	copiarMatriz(matrizImg,matrizAux);
 }
 void traslate(float d,int op)
 {
-	lim[0].traslateP(d,op);
-	lim[1].traslateP(d,op);
-	lim[2].traslateP(d,op);
-	lim[3].traslateP(d,op);
 	int x,y,x2,y2;
 	for(x2=1;x2<=numFil;x2++)
 		for(y2=1;y2<=numCol;y2++)
@@ -122,29 +153,36 @@ void traslate(float d,int op)
 		   	if((x>0) && (y>0) &&(x<=numFil)&&(y<=numCol))
 		      	matrizAux[x2][y2]=matrizImg[x][y];
 		   	else
-		      	matrizAux[x2][y2]=0;//aqui le pones un valor que quieras
+		      	matrizAux[x2][y2]=255;//aqui le pones un valor que quieras
 		}
+	lim[0].traslateP(d,op);
+	lim[1].traslateP(d,op);
+	lim[2].traslateP(d,op);
+	lim[3].traslateP(d,op);		
 	copiarMatriz(matrizImg,matrizAux);   
 }
 void rotate(float t)
 {
-	lim[0].rotateP(t);
-	lim[1].rotateP(t);
-	lim[2].rotateP(t);
-	lim[3].rotateP(t);
+	medidas();		
 	int x,y,x2,y2,cx,cy;
-	cx=numFil/2;
-	cy=numCol/2;
+	float coseno=cos(-t*PI/180.0);
+   	float seno=sin(-t*PI/180.0);
+	cx=(mayorX-menorX)/2 + menorX;
+	cy=(mayorY-menorY)/2  + menorY;
 	for(x2=1;x2<=numFil;x2++)
 		for(y2=1;y2<=numCol;y2++)
 		   {
-			   x=cos(-t*PI/180.0)*(x2-cx) + sin(-t*PI/180.0)*(y2-cy)+cx;
-			   y=cos(-t*PI/180.0)*(y2-cy) - sin(-t*PI/180.0)*(x2-cx)+cy ;
-		   	if((x>0) && (y>0) &&(x<=numFil)&&(y<=numCol))
+			   x=coseno*(x2-cx) + seno*(y2-cy)+cx;
+			   y=coseno*(y2-cy) - seno*(x2-cx)+cy ;
+		   	if((x>=menorX && (y>=menorY)) &&(x<=mayorX&&(y<=mayorY)))
 		      	matrizAux[x2][y2]=matrizImg[x][y];
 		   	else
-		      	matrizAux[x2][y2]=0;//aqui le pones un valor que quieras
+		      	matrizAux[x2][y2]=255;//aqui le pones un valor que quieras
 		   }
+	lim[0].rotateP(t);
+	lim[1].rotateP(t);
+	lim[2].rotateP(t);
+	lim[3].rotateP(t);		
 	copiarMatriz(matrizImg,matrizAux);  
 }
 void procImagem()
@@ -152,32 +190,30 @@ void procImagem()
 	char registro[150];
 	string reg;
     cout<<"--Programa tarefa 4--\n";
-	cout<<"Ingrese nombre del archivo de entrada:";
-    cin>>nomEntrada;
-	nomEntrada+=".pgm";  //Nome de arquivo entrada PGM
+	nomEntrada+="tartaruga.pgm";  //Nome de arquivo entrada PGM
     cout<<"Ele programa esta usando o arquivo "<<nomEntrada<<"\n";	
 	 
 	//O arquivo de teste Ã© aberto para leitura
 	if((fd=fopen(nomEntrada.c_str(),"r"))==NULL)
 	{
-		cout<<"no se puede abrir "<<nomEntrada<<endl;
+		cout<<"Não pode abrir o arquivo "<<nomEntrada<<endl;
 		return;
 	}    
      
-	cout<<"Ingrese nombre del archivo salida:";
+	cout<<"Inserir o nome do arquivo de saída:";
     cin>>nomSalida;
     nomSalida+=".pgm";  //Nome de arquivo saida PGM*/
     
     if(nomEntrada==nomSalida)
     {
-    	cout<<"El nombre del archivo de salda debe ser diferente al del archivo de entrada\n";
+    	cout<<"O nome do arquivo de saída deve ser diferente do arquivo de entrada\n";
     	return;
 	}
 
 	//Ele cria ou sobrescreve o arquivo onde o resultado Ã© salvo
 	if((fs=fopen(nomSalida.c_str(),"w+"))==NULL)
 	{
-	  	cout<<"no se puede abrir "<<nomSalida<<endl;
+	  	cout<<"Não pode abrir o arquivo "<<nomSalida<<endl;
 	  	return;
 	}   
  
@@ -188,23 +224,23 @@ void procImagem()
  	cout<<"tipo:"<<registro;
  	if(registro[0]!='P'||(registro[1]!='2'&&registro[1]!='5'))
  	{
-        cout<<"\nERROR: la imagen de entrada no esta en formato PGM P2\n";
+        cout<<"\nERROR: a imagem de entrada não está no formato PGM P2\n";
         system("pause");
         return ;
  	}
  
-	//LECTURA DE COMENTARIOS
+	//Ler comentários
  	fgets(registro,100,fd);
  	fflush(stdin);   
  	while(registro[0]=='#')
  	{
-     	cout<<"Comentario :"<<registro;                                      
+     	cout<<"Comentário :"<<registro;                                      
      	fputs(registro,fs);  
      	fflush(stdin);   
      	fgets(registro,100,fd);
  	}
  
- 	//LECTURA DE numFil Y numCol
+ 	//Ler DE numFil Y numCol
  	reg=(string)registro;
  	numFil=stoi(reg.substr(0,reg.find(" ")));
  	numCol=stoi(reg.substr(reg.find(" "),reg.size()-reg.find(" ")));
@@ -220,7 +256,7 @@ void procImagem()
  	cout<<"\ngrayscale: "<<grayScale<<endl;   
  	fflush(stdin);
  	
- 	//Lectura de matriz entrada
+ 	//Matriz de entrada de leitura
  	int pixel;   			
  	for(int i=1;i<=numFil;i++)
  	{
@@ -233,7 +269,7 @@ void procImagem()
 	}		
 	copiarMatriz(matrizOriginal,matrizImg);		
 
-	// Lectura de linea de comandos
+	// Lendo linha de comando
 		//  ((\\+|-)?[[:digit:]]+)(\\.[[:digit:]]+)?          NUMERO REAL
 		//  ((\\+|-)?[[:digit:]]+)                            NUMERO ENTERO
 		//  RO\\(((\\+|-)?[[:digit:]]+)(\\.(([[:digit:]]+)?))?\\)    RO()
@@ -242,21 +278,21 @@ void procImagem()
 	
 		//((R((O\\(((\\+|-)?[[:digit:]]+)(\\.(([[:digit:]]+)?))?\\))|H|V))|(M(H|V)\\(((\\+|-)?[[:digit:]]+)\\)))     RO(),MH(),MV(),RH.RV
 	bool band=false;
-    lim[0].create(94,98);
-    lim[1].create(94,246);
-    lim[2].create(274,98);
-    lim[3].create(274,246);
+    lim[0].create(94,96);
+    lim[1].create(94,252);
+    lim[2].create(282,96);
+    lim[3].create(282,252);
 	do{
 		regex sintaxi("(((R((O\\(((\\+|-)?[[:digit:]]+)(\\.(([[:digit:]]+)?))?\\))|H|V))|(M(H|V)\\(((\\+|-)?[[:digit:]]+)\\)));)*((R((O\\(((\\+|-)?[[:digit:]]+)(\\.(([[:digit:]]+)?))?\\))|H|V))|(M(H|V)\\(((\\+|-)?[[:digit:]]+)\\)))");
 		string linea;
 		band=false;
 		//limpiamos el buffer antes de la siguiente lectura
 		//cin.ignore(256,'\n');
-		cout<<"Ingrese una linea de comandos!"<<endl;
+		cout<<"Digite uma linha de comando!"<<endl;
 		getline(cin, linea);
 		if(regex_match(linea,sintaxi))
 		{	
-			cout<<"bien"<<endl;
+			cout<<"good :)"<<endl;
 			linea+=";";
 			do
 			{
@@ -265,7 +301,7 @@ void procImagem()
 				comando=linea.substr(0,pos);
 				linea=linea.substr(pos+1,linea.size()-pos);
 				codigo=comando.substr(0,2);
-				cout<<"subcadena: "<<comando<<endl;
+				cout<<"substring: "<<comando<<endl;
 				if(codigo=="RH")
 					reflexion(1);
 				else if(codigo=="RV")
@@ -284,34 +320,36 @@ void procImagem()
 					{					
 						rotate(numArg);				
 					}
-					cout<<" con argumento : "<<numArg<<endl;	
+					cout<<" com o argumento: "<<numArg<<endl;	
 				}
+				cout<<"menorX: "<<menorX<<" mayorX: "<<mayorX<<" menorY: "<<menorY<<" mayorY: "<<mayorY<<endl;   
 			}
 			while(!linea.empty());		
 			if(lim[0].fueraLimite()&&lim[1].fueraLimite()&&lim[2].fueraLimite()&&lim[3].fueraLimite())	
 			{
 				fflush(stdin);
-				cout<<"La tortuga esta totalmente afuera de la imagen!!\n";
-				cout<<"Quiere ingresar otra linea de comandos? (s/n):";
+				cout<<"A tartaruga é TOTALMENTE FORA do quadro!!\n";
+				cout<<"Ele quer tentar novamente?(s/n):";
 				cin>>linea;
 				if(linea=="s"||linea=="S")
 				{
 					copiarMatriz(matrizImg,matrizOriginal);
-					lim[0].create(94,98);
-    				lim[1].create(94,246);
-    				lim[2].create(274,98);
-    				lim[3].create(274,246);
+				    lim[0].create(94,96);
+				    lim[1].create(94,252);
+				    lim[2].create(282,96);
+				    lim[3].create(282,252);	
 					band=true;
 				}
 				else if(linea=="n"||linea=="N") 
 					band=false;
+				fflush(stdin);
 			}
 
 		}
 		else
 		{
-			cout<<"Error de sintaxis en la linea de comandos ingresada!!!"<<endl;
-			cout<<"Vuelva a intetarlo\n\n";
+			cout<<"Erro de sintaxe na linha de comando introduzido!!!"<<endl;
+			cout<<"Tente de novo\n\n";
 			fflush(stdin);
 			band=true;
 		}
@@ -336,7 +374,7 @@ void procImagem()
     	}
     	fprintf(fs,"%s\n"," ");
  	}  
- 	cout<<"\nFin de procesamiento de la imagen "<<nomEntrada<<endl;
+ 	cout<<"\nFinal de processamento de imagem "<<nomEntrada<<endl;
  	fclose(fs);
  	fclose(fd);
 
@@ -348,4 +386,3 @@ int main()
     getch ();
     return 0;
 }
-//Valores a probar posicion (475 , 20)
